4c688a6d3cd00b6180c08bfa535e66c5817101bb
timothyn <tnguyen_42@yahoo.com>
2025-05-16 15:46:48 -0400

    Finish review and favorite Model

--- models/favorite.js
+++ models/favorite.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const favoriteSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    restaurant: {
+        type: Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: true
+    }
+}, {
+    timestamps: true
+});
 
 
-module.exports = mongoose.model('Restaurant', restaurantSchema);
+module.exports = mongoose.model('Favorite', favoriteSchema);
--- models/review.js
+++ models/review.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
+const restaurant = require('./restaurant');
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+comments: {
+    type: String,
+    required: true,
+},
+rating: {
+    type: Number,
+    min: 1,
+    max: 5
+},
+restaurant: {
+    type: Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true,
+},
+user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+},
+}, {
+    timestamps: true
+});
 
-module.exports = mongoose.model('Restaurant', restaurantSchema);
+module.exports = mongoose.model('Review', reviewSchema);
