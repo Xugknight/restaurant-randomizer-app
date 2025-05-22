acb811df2d752e2ccd7340a721e326123a926fe1
timothyn <tnguyen_42@yahoo.com>
2025-05-21 20:20:16 -0400

    Add back button to some missing pages. Add index/random.ejs for favorites. Can view favorites and randomize from favorites

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -4,6 +4,27 @@ const Favorite = require('../models/favorite');
 const ensureLoggedIn = require('../middleware/ensure-logged-in');
 const restaurant = require('../models/restaurant');
 
+// Index
+// GET /favorites
+router.get('/', ensureLoggedIn, async (req, res) => {
+    const favorites = await Favorite.find({ user: req.user._id }).populate('restaurant');
+    const favoriteRestaurants = favorites.map((favorite) => favorite.restaurant);
+    res.render('favorites/index.ejs', { favoriteRestaurants });
+});
+
+// Random Favorites
+// GET /favorites/random
+router.get('/random', ensureLoggedIn, async (req, res) => {
+    const favorites = await Favorite.find({ user: req.user._id }).populate('restaurant');
+    const favoriteRestaurants = favorites.map((favorite) => favorite.restaurant);
+    if (favoriteRestaurants.length === 0) {
+        return res.redirect('/favorites');
+    }
+    const randomIndex = Math.floor(Math.random() * favoriteRestaurants.length);
+    const randomRestaurant = favoriteRestaurants[randomIndex];
+    res.render('favorites/random.ejs', { restaurant: randomRestaurant });
+});
+
 // Favorite
 // POST /favorites/:restaurantId
 router.post('/:restaurantId', ensureLoggedIn, async (req, res) => {
